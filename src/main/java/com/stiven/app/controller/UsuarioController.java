c8ef2d299f96c26b7f71ee27a956a51074fc491d
Jost2167 <johnstiven2000@gmail.com>
2024-05-21 21:19:11 -0500

    Completando las vista con i18n

--- src/main/java/com/stiven/app/controller/UsuarioController.java
+++ src/main/java/com/stiven/app/controller/UsuarioController.java
@@ -1,17 +1,32 @@
 package com.stiven.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.stiven.app.entity.ResidenciaEntity;
+import com.stiven.app.service.IResidenciaService;
 
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
 
+	@Autowired
+	private IResidenciaService residenciaService;
+	
+	
+	
+	
 	@GetMapping("/ver-reserva")
-	public String verReserva() {
-		return "usuario/ver-reserva";
+	public String verReserva(Model model) {
+		List<ResidenciaEntity> residencias = residenciaService.listAll();
+		model.addAttribute("residencias", residencias);
+		return "index";
 	}
 	
 }
